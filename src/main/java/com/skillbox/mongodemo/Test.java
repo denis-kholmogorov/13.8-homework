7ea81165774198780aacfed3c8b9ae2ca04a224e
Denis <dendiesel88@yandex.ru>
2020-01-15 11:06:44 +0300

    homework 1.1

--- src/main/java/com/skillbox/mongodemo/Test.java
+++ src/main/java/com/skillbox/mongodemo/Test.java
@@ -1,7 +1,6 @@
 package com.skillbox.mongodemo;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DocumentToDBRefTransformer;
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
@@ -20,7 +19,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
-import static com.mongodb.client.model.Filters.*;
 
 public class Test {
 
@@ -71,8 +69,6 @@ public class Test {
         });
     }
 
-
-
     private static List<String[]> getLines(File fileCsv){
         File file = fileCsv;
 
@@ -93,51 +89,3 @@ public class Test {
         return null;
     }
 }
-
-/*
-        MongoDatabase database = mongoClient.getDatabase("local");
-
-        // Создаем коллекцию
-        MongoCollection<Document> collection = database.getCollection("TestSkillDemo");
-
-        // Удалим из нее все документы
-        collection.drop();
-
-        // Создадим первый документ
-        Document firstDocument = new Document()
-                .append("Type", 1)
-                .append("Description", "Это наш первый документ в MongoDB")
-                .append("Author", "Я")
-                .append("Time", new SimpleDateFormat().format(new Date()));
-
-
-        // Вложенный объект
-        Document nestedObject = new Document()
-                .append("Course", "NoSQL Базы Данных")
-                .append("Author", "Mike Ovchinnikov");
-
-        firstDocument.append("Skillbox", nestedObject);
-
-
-        // Вставляем документ в коллекцию
-        collection.insertOne(firstDocument);
-
-        collection.find().forEach((Consumer<Document>) document -> {
-            System.out.println("Наш первый документ:\n" + document);
-        });
-
-        // Используем JSON-синтаксис для создания объекта
-        Document secondDocument = Document.parse(
-                "{Type: 2, Description:\"Мы создали и нашли этот документ с помощью JSON-синтаксиса\"}"
-        );
-        collection.insertOne(secondDocument);
-
-        // Используем JSON-синтаксис для написания запроса (выбираем документы с Type=2)
-        BsonDocument query = BsonDocument.parse("{Type: {$eq: 2}}");
-        collection.find(query).forEach((Consumer<Document>) document -> {
-            System.out.println("Наш второй документ:\n" + document);
-        });
-        }
-    }
-}
-*/
